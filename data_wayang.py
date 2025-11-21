81484fea7b5fa108b9fe921bbbfdb232e3839914
Dwi Aryani <162438100+dwiaryani2@users.noreply.github.com>
2025-11-21 15:01:31 +0700

    Add files via upload

---
+++ data_wayang.py
@@ -0,0 +1,73 @@
+# karakter wayang
+tokoh_wayang = {
+        'anoman': {
+            'nama_wayang': 'Anoman',
+            'nama_panggilan': 'Hanoman',
+            'nama_kecil': 'Maruti',
+            'nama_gelar': 'Raden Anoman',
+            'deskripsi': 'Kera putih sakti, putra Batara Bayu. Anoman memiliki kekuatan luar biasa, bisa berpindah cepat (sepiangin), dan mampu menentukan kematiannya sendiri. Ia dikenal setia, pemberani, dan tokoh penting dalam kisah Ramayana.'
+        },
+        'arjuna': {
+            'nama_wayang': 'Arjuna',
+            'nama_panggilan': 'Janaka',
+            'nama_kecil': 'Permadi',
+            'nama_gelar': 'Dananjaya',
+            'deskripsi': 'Ksatria Pandawa yang pandai memanah, tampan, dan lembut. Nama kecilnya Permadi, dan dikenal juga sebagai Janaka. Arjuna adalah simbol keanggunan, keberanian, dan spiritualitas.'
+        },
+        'bagong': {
+            'nama_wayang': 'Bagong',
+            'nama_panggilan': 'Bagong',
+            'nama_kecil': 'Bayangan Semar',
+            'nama_gelar': 'Bagha / Baqa / Albaghoya',
+            'deskripsi': 'Anak ketiga Semar yang lahir dari bayangannya. Sosok lucu, blak-blakan, dan suka berpura-pura bodoh. Meski terlihat sembarangan, Bagong justru mewakili manusia yang utuh dengan segala kekurangannya.'
+        },
+        'bima': {
+            'nama_wayang': 'Bima',
+            'nama_panggilan': 'Werkudara',
+            'nama_kecil': 'Sena',
+            'nama_gelar': 'Bratasena',
+            'deskripsi': 'Sosok gagah bertubuh besar, teguh pada prinsip, dan tak tergoda kekuasaan. Nama kecilnya Sena, sedangkan Werkudara adalah nama kehormatannya. Bima mewakili kekuatan dan ketulusan.'
+        },
+        'gareng': {
+            'nama_wayang': 'Nala Gareng',
+            'nama_panggilan': 'Gareng',
+            'nama_kecil': 'Nala Khairan',
+            'nama_gelar': 'Gareng Pincang',
+            'deskripsi': 'Anak pertama Semar, bertubuh cacat sebagai simbol filosofi hidup: hati-hati dalam bertindak, sadar bahwa usaha ditentukan Tuhan, dan pentingnya memahami realita hidup.'
+        },
+        'gatotkaca': {
+            'nama_wayang': 'Raden Gatotkaca',
+            'nama_panggilan': 'Lelananging Jagad',
+            'nama_kecil': 'Tetuka',
+            'nama_gelar': 'Prabu Gatotkaca',
+            'deskripsi': 'Putra Bima dan Dewi Arimbi, Raja Pringgadani. Bertulang besi dan berotot kawat, Gatotkaca dikenal sakti dan mampu terbang di angkasa. Ia menjadi perisai Pandawa dalam Baratayuda dan dihormati sebagai ksatria hampir tak terkalahkan.'
+        },
+        'kresna': {
+            'nama_wayang': 'Sri Kresna',
+            'nama_panggilan': 'Kresna',
+            'nama_kecil': 'Narayana',
+            'nama_gelar': 'Prabu Kresna Basudewa',
+            'deskripsi': 'Putra kedua Prabu Basudewa dari Kerajaan Mandura. Kresna adalah titisan Batara Wisnu, dikenal bijaksana, kharismatik, dan sakti. Ia memiliki pusaka Cakra Sudarsana dan mampu berubah menjadi raksasa saat murka. Dalam perang Baratayuda, ia menjadi penasihat utama Pandawa.'
+        },
+        'petruk': {
+            'nama_wayang': 'Petruk',
+            'nama_panggilan': 'Petruk',
+            'nama_kecil': 'Fatruk',
+            'nama_gelar': 'Kanthong Bolong',
+            'deskripsi': 'Anak kedua Semar, bertubuh tinggi dengan pemikiran panjang. Namanya bermakna "tinggalkan", simbol keikhlasan berbagi dan berserah diri. Petruk dikenal jenaka, penyayang, dan suka menolong.'
+        },
+        'puntadewa': {
+            'nama_wayang': 'Yudhistira',
+            'nama_panggilan': 'Puntadewa',
+            'nama_kecil': 'Puntadewa',
+            'nama_gelar': 'Dharmaputra',
+            'deskripsi': 'Anak sulung Pandawa yang sabar, adil, dan jujur. Nama kecilnya Puntadewa yang berarti “teguh dalam peperangan”. Ia menjadi simbol moral dan pemimpin yang mengedepankan etika di tengah konflik.'
+        },
+        'semar': {
+            'nama_wayang': 'Semar',
+            'nama_panggilan': 'Ki Lurah Semar',
+            'nama_kecil': 'Ismoyo',
+            'nama_gelar': 'Simar / Ismarun (simbol paku kehidupan)',
+            'deskripsi': 'Tokoh Punakawan tertua, bijak dan rendah hati. Namanya berarti paku, simbol dari keteguhan. Meski sederhana, Semar punya pikiran jernih, niat kuat untuk mencipta, dan kasih terhadap sesama.'
+        }
+}
